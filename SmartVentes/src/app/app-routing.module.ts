92ae792324ee250d70e2d6cc8d9b93c8de4c0c9c
SamiraBou <amiraboulaaid@gmail.com>
2020-02-27 13:44:49 +0100

    add ather fontionalities

--- SmartVentes/src/app/app-routing.module.ts
+++ SmartVentes/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientComponent } from './gestionVents/client/client.component';
 import { ListClientComponent } from './gestionVents/client/list-client/list-client.component';
+import { FournisseurComponent } from './gestionVents/fournisseur/fournisseur.component';
+import { ProduitComponent } from './gestionVents/produit/produit.component';
 
 
 const routes: Routes = [
-  {path:'', redirectTo:'client', pathMatch:'full'},
+  {path:'', redirectTo:'list_Produit', pathMatch:'full'},
   {path:'client', component:ClientComponent},
   {path:'list-client', component:ListClientComponent},
+  {path:'Fournisseur', component:FournisseurComponent},
+  {path:'list-Fournisseur', component:ListClientComponent},
+  {path:'Produit', component:ProduitComponent},
+  {path:'list-Produit', component:ListClientComponent},
 ];
 
 @NgModule({
